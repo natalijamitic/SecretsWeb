2849dbf61558a8c6681af94f5220cac65c6d3fd2
Natalija Mitic <naca@ntec.ch>
2019-09-18 20:58:06 +0200

    Submit secret

--- app.js
+++ app.js
@@ -32,7 +32,8 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
   googleId: String,
-  facebookId: String
+  facebookId: String,
+  secret: [String]
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -106,11 +107,38 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/secrets", (req, res) => {
+  User.find({"secret": {$ne: null}}, (err, foundUsers) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
+});
+
+app.post("/submit", (req, res) => {
+  const submittedSecret = req.body.secret;
+  User.findById(req.user.id, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret.push(submittedSecret);
+        foundUser.save(() => res.redirect("/secrets"));
+      }
+    }
+  });
+
 });
 
 app.get("/logout", (req, res) => {

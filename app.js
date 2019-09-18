9fa372c12b507d01969b06a7de1feb270b76f10e
Natalija Mitic <naca@ntec.ch>
2019-09-18 18:49:12 +0200

    Register/login with Facebook

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const FacebookStrategy = require("passport-facebook").Strategy;
 const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
@@ -30,7 +31,8 @@ mongoose.connect("mongodb://localhost:27017/userDB", {useFindAndModify: false, u
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
-  googleId: String
+  googleId: String,
+  facebookId: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -61,6 +63,18 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+passport.use(new FacebookStrategy({
+    clientID: process.env.APP_ID,
+    clientSecret: process.env.APP_SECRET,
+    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+  },
+  (accessToken, refreshToken, profile, cb) => {
+    User.findOrCreate({ facebookId: profile.id }, (err, user) => {
+      return cb(err, user);
+    });
+  }
+));
+
 app.get("/", (req, res) => {
   res.render("home");
 });
@@ -73,6 +87,14 @@ app.get("/auth/google/secrets",
   passport.authenticate("google", { failureRedirect: "/login"}),
   (req, res) => {
     res.redirect("/secrets");
+});
+
+app.get('/auth/facebook', passport.authenticate('facebook'));
+
+app.get('/auth/facebook/secrets',
+  passport.authenticate('facebook', { failureRedirect: '/login' }),
+    (req, res) => {
+    res.redirect('/secrets');
 });
 
 app.get("/login", (req, res) => {
